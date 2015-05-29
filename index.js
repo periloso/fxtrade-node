89409793b1beb7b4b2a91f22cd194d6956ace1fb
Chris Kellendonk <chriskellendonk@gmail.com>
2015-05-29 01:37:10 -0400

    remove debug code

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 var Oanda = require("./lib/oanda");
 
-var test = new Oanda('https://api-sandbox.oanda.com/');
-
-test.rates().candles({
-  "instrument": "EUR_USD"
-}).on('complete', function(result, response) {
-  console.log(result);
-});
-
 module.exports = Oanda;
